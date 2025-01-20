60e7be02b540bdde10cbedc31d55b618337f6b90
patrickmac3 <patrickmaceachen9@gmail.com>
2025-01-20 18:51:09 -0500

    GH-44: [feat] Created endpoints for user activity history (events created and participated in)

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/service/event/EventService.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/service/event/EventService.java
@@ -1,5 +1,8 @@
 package app.sportahub.eventservice.service.event;
 
+import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.Page;
+
 import app.sportahub.eventservice.dto.request.EventRequest;
 import app.sportahub.eventservice.dto.response.EventResponse;
 import app.sportahub.eventservice.dto.response.ParticipantResponse;
@@ -21,4 +24,8 @@ public interface EventService {
     void deleteEvent(String id);
 
     ParticipantResponse joinEvent(String id, String userId);
+
+    Page<EventResponse> getEventsByParticipantId(String userId, int page, int size);
+
+    Page<EventResponse> getEventsCreatedByUserId(String userId, int page, int size);
 }
